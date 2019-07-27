c77b0e046c57476225fa99d394c7f4644f6479ed
Enrico Vogt <mail@enricovogt.de>
2019-07-27 23:52:32 +0200

    removed inline css for icon-label

--- src/components/icon-label/icon-label.js
+++ src/components/icon-label/icon-label.js
@@ -4,11 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function IconLabel(props) {
   return (
-    <React.Fragment>
-      <span style={{width: '25px', display:'inline-block', textAlign: 'center'}}>
+    <span class="icon-label">
+      <span>
         <FontAwesomeIcon icon={props.icon} />
       </span>
       {props.children}
-    </React.Fragment>
+    </span>
   );
 }
